bd0e812c76ad19f0d1d2172bc8ebe8fb0e7aef18
Andreas Österreicher <oesi@technikum-wien.at>
2023-04-07 11:44:15 +0200

    Improved Extension Manager GUI and display Server Name

--- application/views/system/extensions/manager.php
+++ application/views/system/extensions/manager.php
@@ -33,7 +33,7 @@
 	<body>
 		<div class="container-fluid">
 			<h3 class="page-header">
-				<?php echo $this->p->t('extensions', 'title'); ?>
+				<?php echo $this->p->t('extensions', 'title').' - '.SERVER_NAME; ?>
 			</h3>
 
 			<div>
@@ -42,33 +42,23 @@
 
 			<?php echo form_open_multipart(current_url().'/uploadExtension'); ?>
 				<div class="row">
-					<div class="col-xs-2">
-						<input type="file" name="extension" />
+					<div class="col-md-4">
+						<input class="form-control" type="file" name="extension" />
 					</div>
-					<div>
-						<input type="button" id="uploadExtension" value="<?php echo $this->p->t('extensions', 'uploadExtension'); ?>" />
+					<div class="col-md-2">
+						<div class="form-check">
+							<input type="checkbox" class="form-check-input" id="notPerformSql" name="notPerformSql"/>
+							<label class="form-check-label" for="notPerformSql"><?php echo $this->p->t('extensions', 'performSql'); ?></label>
+						</div>
 					</div>
-				</div>
-
-				<br/>
-
-				<div class="row">
-					<div class="col-xs-2">
-						<label for="notPerformSql"><?php echo $this->p->t('extensions', 'performSql'); ?></label>
-					</div>
-					<div>
-						<input type="checkbox" class="checkbox" id="notPerformSql" name="notPerformSql"/>
+					<div class="col-md-2">
+						<input type="button" class="btn btn-default" id="uploadExtension" value="<?php echo $this->p->t('extensions', 'uploadExtension'); ?>" />
 					</div>
 				</div>
 			</form>
+			<br /><br /><br /><br /><br /><br /><br /><br />
 		</div>
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
+		
 	</body>
 
 <?php $this->load->view('templates/FHC-Footer'); ?>
